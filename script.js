1bd37c321b34d8269ede2741d0868f4849282c4f
arewhatyouit <isaac.noyes@gmail.com>
2025-01-21 23:25:23 -0800

    End of day update.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
+// Rock Paper Scissors
+
 const prompt = require("prompt-sync")({ sigint: true });
 let humanScore = 0;
 let computerScore = 0;
 
-// Rock Paper Scissors
-// Randomly generate a prime number between 1-3
-// Create a function called getComputerChoice which returns Rock, Paper or Scissors based on the number generated
+// Randomly generate a prime number between 1-3 then Create a function called getComputerChoice which returns Rock, Paper or Scissors based on the number generated
 
 function getComputerChoice() {
   let computerChoice = "";
@@ -24,6 +24,7 @@ let computerChoice = getComputerChoice();
 console.log("Lets play rock, paper scissors!");
 
 // Create a function called getHumanChoice which uses prompt input to get the users choice or Rock Paper of Scissors
+
 function getHumanChoice() {
   let humanChoice = prompt(
     "Choose rock, paper or scissors. ",
@@ -47,60 +48,86 @@ let humanChoice = getHumanChoice();
 // Function to reset game if choices are the same.
 
 function resetGame() {
-  console.log("Testing reset condition.")
   let resetPrompt = prompt(`You both chose ${humanChoice}, choose again. `, "");
-  console.log(resetPrompt);
   return {
     humanChoice: resetPrompt,
     computerChoice: getComputerChoice()
   };
 }
 
-humanChoice = resetGame().humanChoice;
-computerChoice = resetGame().computerChoice;
+// Creat a function comparing the Human and Computers choice. If the Human wins, return "You win!" else return "You lose!"
 
-// Creat a function comparing the Human and Computers choice. If the Human wins, return "You win!" else return "You loose!"
-
-function playRound() {
+function playRound(humanChoice, computerChoice) {
   let outcome = "";
+  let scoreBoolean = null;
 
   if (humanChoice === computerChoice) {
-    console.log("Checking tie condition");
-    let result = resetGame();
-    humanChoice = result.humanChoice;
-    computerChoice = result.computerChoice;
+    let roundResult = resetGame();
+    humanChoice = roundResult.humanChoice;
+    computerChoice = roundResult.computerChoice;
   } else if (humanChoice === "rock" && computerChoice === "paper") {
-    console.log("Checking Rock vs Paper condition...");
     outcome = `Computer chose ${computerChoice}, ${computerChoice} beats ${humanChoice}. You lose.`;
+    scoreBoolean = false;
   } else if (humanChoice === "paper" && computerChoice === "rock") {
-    console.log("Checking Paper vs Rock condition...");
     outcome = `You chose ${humanChoice}, ${humanChoice} beats ${computerChoice}. You win!`;
+    scoreBoolean = true;
   } else if (humanChoice === "paper" && computerChoice === "scissors") {
-    console.log("Checking Paper vs Scissors condition...");
     outcome = `Computer chose ${computerChoice}, ${computerChoice} beats ${humanChoice}. You lose.`;
+    scoreBoolean = false;
   } else if (humanChoice === "scissors" && computerChoice === "paper") {
-    console.log("Checking Scissors vs Paper condition...");
     outcome = `You chose ${humanChoice}, ${humanChoice} beats ${computerChoice}. You win!`;
+    scoreBoolean = true;
   } else if (humanChoice === "scissors" && computerChoice === "rock") {
-    console.log("Checking Scissors vs Rock condition...");
     outcome = `Computer chose ${computerChoice}, ${computerChoice} beats ${humanChoice}. You lose.`;
+    scoreBoolean = false;
   } else if (humanChoice === "rock" && computerChoice === "scissors") {
-    console.log("Checking Rock vs Scissors condition...");
     outcome = `You chose ${humanChoice}, ${humanChoice} beats ${computerChoice}. You win!`;
+    scoreBoolean = true;
   }
 
-  return outcome;
+  return {
+    outcome: outcome,
+    scoreBoolean: scoreBoolean
+  };
 }
-let outcome = playRound();
-console.log(outcome);
 
-// function playAgain() {
-//   console.log("Play again?");
-//   playRound();
-// }
+//Create a function to add +1 to whomever wins the round
 
-// console.log(playRound());
-// console.log(playAgain());
+function score() {
+  let result = playRound();
+  if (result.scoreBoolean === true) {
+    humanScore++;
+  } else {
+    computerScore++;
+  }
 
-// Create a function adding one point to the Humans score, else add 1 point to the Computers score
-// Start another round
+  return {
+    humanScore: humanScore,
+    computerScore: computerScore
+  };
+}
+
+//Function to display the score
+
+function displayScore() {
+  let scoreResult = score();
+  let scoreString = `Human: ${scoreResult.humanScore}, Computer: ${scoreResult.computerScore}`;
+  return scoreString;
+}
+
+console.log(displayScore());
+
+//Function to reset the game
+
+function playAgain() {
+  let resetPrompt = prompt("Play again? y/n ", "");
+  if (resetPrompt === "y") {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+  } else {
+    process.exit();
+  }
+}
+
+console.log(playAgain());
