98f0ed474f119bec275e500855ad76d269bd9474
arewhatyouit <isaac.noyes@gmail.com>
2025-03-03 22:04:34 -0800

    Finished all UI coding

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-// Rock Paper Scissors Game
-
-const prompt = require("prompt-sync")({ sigint: true });
+// Rock Paper Scissors Game, now with a UI!
 
 let humanScore = 0;
 let computerScore = 0;
 let roundPlayed = 0;
 const TOTALROUNDS = 5;
 
-//Function to randomly generate a prime number between 1-3 then Create a function called getComputerChoice which returns Rock, Paper or Scissors based on the number generated
+// Assign event listeners for buttons
+
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+
+rockBtn.addEventListener("click", () => {
+  getHumanChoice("rock");
+});
+paperBtn.addEventListener("click", () => {
+  getHumanChoice("paper");
+});
+scissorsBtn.addEventListener("click", () => {
+  getHumanChoice("scissors");
+});
+
+// Function to get computer's choice
 function getComputerChoice() {
   let computerChoice = "";
   let randomNum = Math.floor(Math.random() * 3) + 1;
@@ -18,160 +32,98 @@ function getComputerChoice() {
   } else {
     computerChoice = "scissors";
   }
-  // console.log("Lets play rock, paper scissors!");
   return computerChoice;
 }
 
-//Function called getHumanChoice which uses prompt input to get the users choice or Rock Paper of Scissors
-
-function getHumanChoice() {
-  let humanChoice = prompt(
-    "Choose rock, paper or scissors. ",
-    ""
-  ).toLowerCase();
-  if (
-    humanChoice === "rock" ||
-    humanChoice === "paper" ||
-    humanChoice === "scissors"
-  ) {
-    console.log(`You chose ${humanChoice}!`);
-    return humanChoice;
-  } else {
-    console.log("Invalid choice. Please enter rock, paper, or scissors.");
-    return getHumanChoice();
+// Function to get human's choice, also calls getComputerChoice()
+function getHumanChoice(choice) {
+  if (humanScore + computerScore >= 5) {
+    playAgain();
   }
+
+  document.querySelector("#announce2").textContent = "";
+  const computerChoice = getComputerChoice();
+  console.log(computerChoice);
+  console.log(`Button clicked: ${choice}`);
+  playRound(choice, computerChoice);
 }
 
-//Function to start game
-
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    console.log(`Debug Rounds played = ${roundPlayed}`)
-    console.log(`Round ${i + 1} of ${TOTALROUNDS}`);
-    let firstComputerChoice = getComputerChoice();
-    let firstHumanChoice = getHumanChoice();
-    let result = playRound(firstHumanChoice, firstComputerChoice);
-    console.log(result.outcome); // Show who won
-    let currentScore = score(result); // Update the score
-    console.log(displayScore(currentScore)); // Show the score
-    console.log("")
-    roundPlayed++;
-}
-  if (roundPlayed === TOTALROUNDS) {
-    console.log(`Game over! The final scores are... Human: ${humanScore}, Computer: ${computerScore}`);
-    console.log("")
-}
-
-   playAgain();
-}
-
-playGame();
-
-
-// Function comparing the Human and Computers choice. If the Human wins, return "You win!" else return "You lose!"
+// Function comparing the Human and Computers choice. If the Human wins, return "You win!" else return "You lose!", updates score, updates round
 
 function playRound(humanChoice, computerChoice) {
-  let outcome = "";
   let scoreBoolean = null;
 
   if (humanChoice === computerChoice) {
-    let reset = resetGame(humanChoice, computerChoice);
-    return playRound(reset.humanChoice, reset.computerChoice);
+    document.querySelector("#announce1").textContent =
+      "It's a tie! Choose again.";
+    document.querySelector("#announce2").textContent = "";
+    return;
   } else if (humanChoice === "rock" && computerChoice === "paper") {
-    outcome = `Computer chose ${computerChoice}, ${computerChoice} beats ${humanChoice}. You lose.`;
+    document.querySelector(
+      "#announce1"
+    ).textContent = `Computer chose ${computerChoice}, ${computerChoice} beats ${humanChoice}. You lose the round.`;
     scoreBoolean = false;
   } else if (humanChoice === "paper" && computerChoice === "rock") {
-    outcome = `You chose ${humanChoice}, ${humanChoice} beats ${computerChoice}. You win!`;
+    document.querySelector(
+      "#announce1"
+    ).textContent = `You chose ${humanChoice}, ${humanChoice} beats ${computerChoice}. You win the round!`;
     scoreBoolean = true;
   } else if (humanChoice === "paper" && computerChoice === "scissors") {
-    outcome = `Computer chose ${computerChoice}, ${computerChoice} beats ${humanChoice}. You lose.`;
+    document.querySelector(
+      "#announce1"
+    ).textContent = `Computer chose ${computerChoice}, ${computerChoice} beats ${humanChoice}. You lose the round.`;
     scoreBoolean = false;
   } else if (humanChoice === "scissors" && computerChoice === "paper") {
-    outcome = `You chose ${humanChoice}, ${humanChoice} beats ${computerChoice}. You win!`;
+    document.querySelector(
+      "#announce1"
+    ).textContent = `You chose ${humanChoice}, ${humanChoice} beats ${computerChoice}. You win the round!`;
     scoreBoolean = true;
   } else if (humanChoice === "scissors" && computerChoice === "rock") {
-    outcome = `Computer chose ${computerChoice}, ${computerChoice} beats ${humanChoice}. You lose.`;
+    document.querySelector(
+      "#announce1"
+    ).textContent = `Computer chose ${computerChoice}, ${computerChoice} beats ${humanChoice}. You lose the round.`;
     scoreBoolean = false;
   } else if (humanChoice === "rock" && computerChoice === "scissors") {
-    outcome = `You chose ${humanChoice}, ${humanChoice} beats ${computerChoice}. You win!`;
+    document.querySelector(
+      "#announce1"
+    ).textContent = `You chose ${humanChoice}, ${humanChoice} beats ${computerChoice}. You win the round!`;
     scoreBoolean = true;
   }
 
-  return {
-    outcome: outcome,
-    scoreBoolean: scoreBoolean,
-  };
-}
-
-// Function to reset game if choices are the same.
-
-function resetGame(humanChoice, computerChoice) {
-  let resetPrompt = prompt(
-    `You both chose ${humanChoice}, choose again. `,
-    ""
-  ).toLocaleLowerCase();
-  humanChoice = resetPrompt;
-
-  if (
-    resetPrompt === "rock" ||
-    resetPrompt === "paper" ||
-    resetPrompt === "scissors"
-  ) {
-    humanChoice = resetPrompt;
-    let randomNum = Math.floor(Math.random() * 3) + 1;
-
-    if (randomNum === 1) {
-      computerChoice = "rock";
-    } else if (randomNum === 2) {
-      computerChoice = "paper";
-    } else {
-      computerChoice = "scissors";
-    }
-
-    return {
-      humanChoice: humanChoice,
-      computerChoice: computerChoice,
-    };
-  } else {
-    console.log("Invalid choice. Please enter rock, paper, or scissors.");
-    return resetGame(humanChoice, computerChoice);
-  }
-}
-
-//Create a function to add +1 to whomever wins the round
-
-function score(result) {
-  if (result.scoreBoolean === true) {
+  if (scoreBoolean === true) {
     humanScore++;
-  } else {
+    roundPlayed++;
+  } else if (scoreBoolean === false) {
     computerScore++;
+    roundPlayed++;
   }
-  return {
-    humanScore: humanScore,
-    computerScore: computerScore,
-  };
+
+  document.querySelector(
+    "#round-num"
+  ).textContent = `Round ${roundPlayed} of ${TOTALROUNDS}`;
+
+  document.querySelector(
+    "#score"
+  ).textContent = `Human: ${humanScore} Computer: ${computerScore}`;
+
+  if (humanScore + computerScore === 5 && humanScore > computerScore) {
+    document.querySelector("#announce1").textContent =
+      "You won the game! Choose again to start a new game.";
+    return;
+  } else if (humanScore + computerScore === 5 && humanScore < computerScore) {
+    document.querySelector("#announce1").textContent =
+      "You lost the game. Choose again to start a new game.";
+    return;
+  }
 }
 
-//Function to display the score
-
-function displayScore(scoreResult) {
-  let scoreString = `Human: ${scoreResult.humanScore}, Computer: ${scoreResult.computerScore}`;
-  return scoreString;
-}
-
-//Function to reset the game
-
+// Function to reset game at end of a 5 round game
 function playAgain() {
-  let resetPrompt = prompt("Play again? y/n ", "");
-  if (resetPrompt === "y") {
-    humanChoice = 0;
-    computerChoice = 0;
-    roundPlayed = 0;
-    playGame();
-  } else if (resetPrompt === "n") {
-    process.exit();
-  } else {
-    console.log("Invalid choice. Please enter 'y' or 'n'");
-    playAgain();
-  }
+  computerScore = 0;
+  humanScore = 0;
+  roundPlayed = 0;
+  document.querySelector(
+    "#round-num"
+  ).textContent = `Round ${roundPlayed} of ${TOTALROUNDS}`;
+  document.querySelector("#score").textContent = "Human: 0 Computer: 0";
 }
